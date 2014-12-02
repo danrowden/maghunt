7eda9381859c5a80017d645821a52554325d4db8
Dan <hello@danrowden.com>
2014-12-02 23:53:44 +0200

    Removed ignored files

--- main/settings/__init__.py
+++
@@ -1,9 +0,0 @@
-# This file is not versioned
-# Setup taken from http://stackoverflow.com/a/15315143
-
-from __future__ import absolute_import
-from .dev import *
-
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '-*-sm64cfeu4^r^oevhk#3x@kqefr0l-#5gblb82wh)oe73=&e'
