eefc3fb247e6f1100f876423554209ab24ed37d4
Dan <hello@danrowden.com>
2014-12-02 21:30:43 +0200

    Start on user profile

--- api/models.py
+++ api/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
+from sorl.thumbnail import ImageField
+import helpers
 
-# Create your models here.
+def get_image_path(instance, filename):
+    basename, format = os.path.splitext(filename)
+    new_filename = instance.user.id + format
+    return os.path.join('avatars', new_filename)
+
+class Profile(models.Model):
+    user                    = models.OneToOneField(User)
+    twitter_username        = models.CharField(max_length=256)
+    twitter_key             = models.CharField(max_length=256)
+    avatar                  = ImageField(upload_to=get_image_path)
+    job_title               = models.CharField(max_length=256)
